f210e62897604e7978f13775ce94cb62658d853f
Jaap Versteegh <j.r.versteegh@gmail.com>
2018-07-25 18:15:45 +0200

    Start with testing environment

---
+++ test/asio_buffer_test.cpp
@@ -0,0 +1,23 @@
+#define BOOST_TEST_MODULE asio_buffer_test
+#include <boost/test/unit_test.hpp>
+
+#include <boost/asio/streambuf.hpp>
+#include <ostream>
+#include <sstream>
+#include <string>
+
+
+BOOST_AUTO_TEST_CASE(consumption_test)
+{
+  boost::asio::streambuf b;
+  std::ostream os(&b);
+  os << "Hello, World!\nThis is me.";
+  b.consume(7);
+  std::string str;
+  std::istream is(&b);
+  is >> str;
+  BOOST_TEST(str == "World!");
+  std::stringbuf sb;
+  is >> std::skipws >> &sb;
+  BOOST_TEST(sb.str() == "This is me.");
+}
