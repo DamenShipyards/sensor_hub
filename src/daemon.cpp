a8ea4e40f81be192f89dd19f28537ec27fbad3e2
Jaap Versteegh <j.r.versteegh@gmail.com>
2018-08-08 17:54:18 +0200

    Extend linux main proc

--- src/daemon.cpp
+++ src/daemon.cpp
@@ -1,4 +1,17 @@
-int main()
+#include "log.h"
+
+#include <boost/filesystem.hpp>
+
+namespace fs = boost::filesystem;
+using pth = boost::filesystem::path;
+
+int main(int argc, char* argv[])
 {
+  pth p{argv[0]};
+  p = fs::canonical(p);
+
+  log(level::info, "Starting: %", p);
+
+  
   return 0;
 }
