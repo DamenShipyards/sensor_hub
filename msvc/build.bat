e7e7bc0873b647abe4cc93d8ddaa41b25502935a
Jaap Versteegh <j.r.versteegh@orca-st.com>
2018-08-16 22:32:39 +0200

    Add command line build script

---
+++ msvc/build.bat
@@ -0,0 +1,16 @@
+set VCVARS="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+set EXE="Release\sensor_hub.exe"
+set DESCRIPTION="Damen Sensor Hub"
+if not '%SIGNSHA1%'=='' goto havekey
+set SIGNSHA1=742ECBBE6C66BB716C80C1FEA18FAB8FFEBC15BA
+:havekey
+
+rem Get visual studio environment
+call %VCVARS%
+rem Build the project
+msbuild sensor_hub.sln
+
+rem Sign ...
+"c:\Program Files\Microsoft SDKs\Windows\v7.1\bin\signtool.exe" sign /sha1 %SIGNSHA1% /d "%DESCRIPTION%" %EXE%
+rem ... and timestamp
+"c:\Program Files\Microsoft SDKs\Windows\v7.1\bin\signtool.exe" timestamp /t http://timestamp.globalsign.com/scripts/timestamp.dll %EXE%
