3d6eb240aa93b9b4c95a50da100e07a3dbc428e9
Jaap Versteegh <j.r.versteegh@orca-st.com>
2018-08-26 00:02:20 +0200

    Start module for device base class

---
+++ src/device.h
@@ -0,0 +1,12 @@
+/**
+ * \file device.h
+ * \brief Provide interface to device base class
+ *
+ * \author J.R. Versteegh <j.r.versteegh@orca-st.com>
+ * \copyright
+ * (C) 2018 Damen Shipyards. All rights reserved.
+ * \license
+ * This software is proprietary. Any use without written
+ * permission from the copyright holder is strictly 
+ * forbidden.
+ */
