bd5ac41bfc9f79b31adfb4ba73f351d74e195241
Jaap Versteegh <j.r.versteegh@gmail.com>
2018-10-18 13:58:36 +0200

    Add modbus inspection tool

---
+++ tools/inspect_modbus.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import time
+from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+
+if len(sys.argv) > 1:
+    ip = sys.argv[1]
+else:
+    ip = '127.0.0.1'
+
+if len(sys.argv) > 2:
+    port = int(sys.argv[2])
+else:
+    port = 16502
+   
+
+client = ModbusClient(ip, port)
+
+while True:
+    request = client.read_input_registers(0x00, 100, unit=0)
+    print(request.registers)
+    time.sleep(4)
