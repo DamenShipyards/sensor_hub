c69764fa618818faad71007e3ab3970b0daf152c
Jaap Versteegh <j.r.versteegh@gmail.com>
2019-08-15 13:29:33 +0200

    Fix path existence check of serial device

--- test/test_ublox.cpp
+++ test/test_ublox.cpp
@@ -1,6 +1,6 @@
 #define BOOST_TEST_MODULE ublox_test
-#include "../src/devices/ublox.h" 
-#include "../src/serial.h" 
+#include "../src/devices/ublox.h"
+#include "../src/serial.h"
 
 #include "test_common.h"
 
@@ -12,8 +12,9 @@
 using namespace ubx;
 namespace fs = boost::filesystem;
 
-const std::string path_0 = "/dev/sensor_hub/ublox_neo_m8u-ttyACM0:921600";
-const std::string path_1 = "/dev/sensor_hub/ublox_neo_m8u-ttyACM1:921600";
+const std::string path_0 = "/dev/sensor_hub/ublox_neo_m8u-ttyACM0";
+const std::string path_1 = "/dev/sensor_hub/ublox_neo_m8u-ttyACM1";
+const std::string baud_string = ":921600";
 
 BOOST_AUTO_TEST_CASE(construction_test) {
   Device_ptr dev = std::make_unique<NEO_M8U<Serial, Ctx> >();
@@ -22,7 +23,7 @@ BOOST_AUTO_TEST_CASE(construction_test) {
 BOOST_AUTO_TEST_CASE(connection_test, *ut::precondition(ublox_available)) {
   asio::io_context& ctx = Ctx::get_context();
   NEO_M8U<Serial, Ctx> ublox;
-  asio::deadline_timer tmr(ctx, posix_time::milliseconds(1000));
+  asio::deadline_timer tmr(ctx, posix_time::milliseconds(2000));
   tmr.async_wait(
       [&ctx](boost::system::error_code ec) {
         ctx.stop();
@@ -31,12 +32,17 @@ BOOST_AUTO_TEST_CASE(connection_test, *ut::precondition(ublox_available)) {
 
   ublox.set_name("ublox-test");
   if (fs::exists(path_0)) {
-    ublox.set_connection_string(path_0);
-  } else {
-    ublox.set_connection_string(path_1);
+    ublox.set_connection_string(path_0 + baud_string);
   }
+  else if (fs::exists(path_1)) {
+    ublox.set_connection_string(path_1 + baud_string);
+  }
+  else {
+    BOOST_TEST(false, "No path to serial device could be found");
+  }
+
   BOOST_TEST(!ublox.is_connected());
-  asio::spawn(ctx, 
+  asio::spawn(ctx,
       [&](asio::yield_context yield) {
         ublox.connect(yield);
       }
