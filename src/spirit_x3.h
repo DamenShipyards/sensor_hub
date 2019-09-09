f9374032c8260fd16ae830c7db9b6b7d5f36de66
Jaap Versteegh <j.r.versteegh@orca-st.com>
2019-09-09 23:46:03 +0200

    Fix endian conversion for boost 1.71

--- src/spirit_x3.h
+++ src/spirit_x3.h
@@ -15,21 +15,87 @@
 #define SPIRIT_X3_H_
 
 #include <cstdint>
+#include <type_traits>
 
 #include <boost/config.hpp>
 #include <boost/endian/detail/intrinsic.hpp>
+#include <boost/version.hpp>
+
+#if BOOST_VERSION >= 107100
+
+/*
+Boost 1.71 is supposed to support double and float, but I don't see how.
+It still exclitly checks whether the type is integral in endian_reverse.
+
+It also provides and additional is_endian_reversible, which looks like
+doing the same thing twice. .. but we can specialize that.
+Seems like the BOOST_STATIC_ASSERTS should check that and not std::integral.
+*/
+
+
+#include <boost/endian/conversion.hpp>
+
+namespace boost { namespace endian {
+
+namespace detail {
+
+template<> struct is_endian_reversible<float>: std::integral_constant<bool, true> {};
+template<> struct is_endian_reversible<double>: std::integral_constant<bool, true> {};
+
+}
+
+template<> inline float endian_reverse<float>(float x) BOOST_NOEXCEPT {
+#ifdef BOOST_ENDIAN_NO_INTRINSICS
+  uint32_t step16 = x << 16 | x >> 16;
+  uint32_t result = ((step16 << 8) & 0xff00ff00) | ((step16 >> 8) & 0x00ff00ff);
+#else
+  uint32_t result = BOOST_ENDIAN_INTRINSIC_BYTE_SWAP_4(*reinterpret_cast<uint32_t*>(&x));
+  return *reinterpret_cast<float*>(&result);
+#endif
+}
+
+template<> inline double endian_reverse<double>(double x) BOOST_NOEXCEPT {
+#ifdef BOOST_ENDIAN_NO_INTRINSICS
+  uint64_t step32 = x << 32 | x >> 32;
+  uint64_t step16 = (step32 & 0x0000FFFF0000FFFFULL) << 16 | (step32 & 0xFFFF0000FFFF0000ULL) >> 16;
+  uint64_t result = (step16 & 0x00FF00FF00FF00FFULL) << 8 | (step16 & 0xFF00FF00FF00FF00ULL) >> 8;
+#else
+  uint64_t result = BOOST_ENDIAN_INTRINSIC_BYTE_SWAP_8(*reinterpret_cast<uint64_t*>(&x));
+#endif
+  return *reinterpret_cast<double*>(&result);
+}
+
+}}
+
+#else
+
 // Inject missing endian_reverse into boost::endian namepace so floating point parsing in spirit works
 namespace boost { namespace endian {
 inline float endian_reverse(float x) BOOST_NOEXCEPT {
+#ifdef BOOST_ENDIAN_NO_INTRINSICS
+  uint32_t step16 = x << 16 | x >> 16;
+  uint32_t result = ((step16 << 8) & 0xff00ff00) | ((step16 >> 8) & 0x00ff00ff);
+#else
   uint32_t result = BOOST_ENDIAN_INTRINSIC_BYTE_SWAP_4(*reinterpret_cast<uint32_t*>(&x));
   return *reinterpret_cast<float*>(&result);
+#endif
 }
+
 inline double endian_reverse(double x) BOOST_NOEXCEPT {
+#ifdef BOOST_ENDIAN_NO_INTRINSICS
+  uint64_t step32 = x << 32 | x >> 32;
+  uint64_t step16 = (step32 & 0x0000FFFF0000FFFFULL) << 16 | (step32 & 0xFFFF0000FFFF0000ULL) >> 16;
+  uint64_t result = (step16 & 0x00FF00FF00FF00FFULL) << 8 | (step16 & 0xFF00FF00FF00FF00ULL) >> 8;
+#else
   uint64_t result = BOOST_ENDIAN_INTRINSIC_BYTE_SWAP_8(*reinterpret_cast<uint64_t*>(&x));
+#endif
   return *reinterpret_cast<double*>(&result);
 }
 }}
+
 #include <boost/endian/conversion.hpp>
+#endif // if BOOST_VERSION >= 107100
+
 #define BOOST_ENDIAN_HAS_FLOATING_POINT
 #include <boost/spirit/home/x3.hpp>
 #include <boost/spirit/home/x3/binary/binary.hpp>
