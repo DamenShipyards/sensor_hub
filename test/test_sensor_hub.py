1ea5ee2610cb322b137c32331991434d2b645ffe
Jaap Versteegh <j.r.versteegh@orca-st.com>
2019-09-09 02:32:31 +0200

    Extend modbus test script

--- test/test_sensor_hub.py
+++ test/test_sensor_hub.py
@@ -4,9 +4,20 @@ import sys
 import pymodbus.client.sync
 import json
 
+if len(sys.argv) > 1:
+    port = int(sys.argv[1])
+else:
+    port = 16502
 
-client = pymodbus.client.sync.ModbusTcpClient('127.0.0.1', 10502)
-resp = client.read_input_registers(0, 40, unit=0)
-for reg in resp.registers:
-    print(reg)
+client = pymodbus.client.sync.ModbusTcpClient('127.0.0.1', port)
+
+def print_device(device):
+    print()
+    print('Device: %d' % device)
+    resp = client.read_input_registers(0, 40, unit=device)
+    for reg in resp.registers:
+        print(reg)
+
+print_device(0)
+print_device(1)
 client.close()
