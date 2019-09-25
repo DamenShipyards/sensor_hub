b319ee469212e3dd5a587651f897ab5aac0a28ab
Jaap Versteegh <j.r.versteegh@orca-st.com>
2019-09-25 12:11:26 +0200

    Update file headers

--- src/device.h
+++ src/device.h
@@ -4,12 +4,21 @@
  *
  * \author J.R. Versteegh <j.r.versteegh@orca-st.com>
  * \copyright
- * (C) 2018 Damen Shipyards. All rights reserved.
+ * Copyright (C) 2019 Damen Shipyards
  * \license
- * This software is proprietary. Any use without written
- * permission from the copyright holder is strictly
- * forbidden.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+
 
 #ifndef DEVICE_H_
 #define DEVICE_H_
