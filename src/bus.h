ff95ff0a53894af6756b2a984d83ef54da4bc190
Jaap Versteegh <j.r.versteegh@gmail.com>
2020-01-04 00:23:04 +0100

    Start bus interface

---
+++ src/bus.h
@@ -0,0 +1,29 @@
+/**
+ * \file bus.h
+ * \brief Provide asio objects for commucation with bus interfaces
+ *
+ * \author J.R. Versteegh <j.r.versteegh@orca-st.com>
+ * \copyright
+ * Copyright (C) 2020 Orca Software
+ * \license
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef BUS_H_
+#define BUS_H_
+
+struct Bus {
+};
+
+#endif  // #define BUS_H_
+// vim: autoindent syntax=cpp expandtab tabstop=2 softtabstop=2 shiftwidth=2
