ed20247c3f15857f801d8ec6df4b49de2855f99c
Jaap Versteegh <j.r.versteegh@gmail.com>
2020-02-28 10:24:09 +0100

    Release 0.4.16

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@
 d=`dirname $0`
 d=`readlink -f "$d"`
 cd "$d"
+version=`awk -F= '/Version/ { print $2 }' version.ini | awk -F. '{ print $1"."$2"."$3 }'`
+if ! grep $version debian/changelog >/dev/null; then
+  echo "debian/changelog does not appear to be up to date.";
+  exit 1;
+fi
 dpkg-buildpackage -b -us -uc 
 mv "$d/../sensor-hub_"* "$d/build/"
