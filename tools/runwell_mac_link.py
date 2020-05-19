7ca360eea78703a53552d263b6f4e2de2650233e
Jaap Versteegh <j.r.versteegh@orca-st.com>
2020-05-20 00:07:41 +0200

    Fix Runwell permanent dev paths

--- tools/runwell_mac_link.py
+++ tools/runwell_mac_link.py
@@ -6,11 +6,11 @@ import sys
 try:
     import serial
 except ImportError:
-    print('Failed: requires pyserial.')
+    print('Failed: requires pyserial.', file=sys.stderr)
     sys.exit(1)
 
 if len(sys.argv) < 2:
-    print('Usage: runwell_mac_link.py <path_to_serial_device>.')
+    print('Usage: runwell_mac_link.py <path_to_serial_device>.', file=sys.stderr)
     sys.exit(2)
 
 dev = sys.argv[1]
@@ -21,7 +21,7 @@ s.port = dev
 try:
     s.open()
 except Exception as e:
-    print('Failed to connect to runwell on %s: %s' % (dev, e))
+    print('Failed to connect to runwell on %s: %s' % (dev, e), file=sys.stderr)
     sys.exit(3)
 else:
     s.reset_input_buffer()
@@ -29,11 +29,7 @@ else:
     s.flush()
     mac = s.read(size=17).decode('utf-8')
     if len(mac) != 17:
-        print('Read operation timed out. Received only %d bytes' % len(mac))
+        print('Read operation timed out. Received only %d bytes' % len(mac), file=sys.stderr)
         sys.exit(4)
     mac = mac.replace(':', '').replace(' ', '')
-    mac_path = '/dev/sensor_hub/runwell_%s' % mac
-    os.system('ln -sf "%s" "%s"' % (dev, mac_path))
-
-
-
+    print(mac)
